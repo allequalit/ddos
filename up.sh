26d0fde6488143c70695a7ae561cb43adca8b693
allequalit <50491988+allequalit@users.noreply.github.com>
2020-04-15 23:33:48 +0900

    Update up.sh

--- up.sh
+++ up.sh
@@ -9,8 +9,8 @@ exit
 fi
 done
 
-mkdir /etc/bu/ 
-cp /etc/sysctl.conf /etc/bu/ & cp /etc/iptables/rules.v4 /etc/bu/
+mkdir /etc/backup/;
+cp /etc/sysctl.conf /etc/backup/;cp /etc/iptables/rules.v4 /etc/backup/
 
 # sysctl.conf (리눅스 커널 설정 파일), rules.v4 (iptables 설정 파일)을 /etc/bu 폴더에 백업합니다.
 # 백업 폴더 위치 : /etc/bu/
@@ -20,11 +20,11 @@ apt-get upgrade -y
 
 # 업데이트 목록 갱신 및 업그레이드 가능한 모든 패키지 업그레이드등을 수행합니다,
 
-apt-get install git -y & git clone https://github.com/allequalit/dp.git
+apt-get install git -y & git clone https://github.com/allequalit/ddos.git
 
 # git 프로그램 설치 및 아트디완s 소스코드 다운로드
 
 mv dp /etc/
-chmod +x /etc/dp/up.sh & chmod +x /etc/dp/start.sh
+chmod +x /etc/ddos/up.sh & chmod +x /etc/ddos/start.sh
 
 # 아트디완s 소스코드를 /etc/dp/ 디렉토리로 이동, start.sh, up.sh에 실행권한 부여
