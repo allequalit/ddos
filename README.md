92f2908a7ffdfba029aaa23cca3913232914a77c
allequalit <50491988+allequalit@users.noreply.github.com>
2020-04-23 22:03:46 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -21,21 +21,19 @@ sudo sh start.sh <br><br>
 # 한국어 (Korean)
 
 # 마냐디완 S PRO (마냐 디도스 완화 .sh PRO) 
-한번의 실행으로 디도스에 많이 사용되는 중국, O**, 아** 웹***, C**P* 아이피주소 차단, 디도스 완화, 네트워크 최적화, tcpbbr (https://cloudblog.withgoogle.com/products/gcp/tcp-bbr-congestion-control-comes-to-gcp-your-internet-just-got-faster/amp/) 등을 적용해주는 프로그램(.sh) 입니다. <br><br>
+한번의 실행으로 디도스에 많이 사용되는 중국, 프랑스, 브라질, O**, re*****es***, 알*** 클***, 아이피주소 차단, 디도스 완화, Zram, 네트워크 최적화, tcpbbr (https://cloudblog.withgoogle.com/products/gcp/tcp-bbr-congestion-control-comes-to-gcp-your-internet-just-got-faster/amp/) 등을 적용해주는 프로그램(.sh) 입니다. <br><br>
 마냐디완 s LIGHT 버전에 비해 무겁지만 방어성능이 뛰어납니다. <br><br>
-프로그램은 오픈소스이며 비상업적, 상업적 용도로 사용가능하지만 재배포는 금지되어있습니다. <br><br>
+프로그램은 오픈소스이며 비상업적, 상업적 용도로 사용가능하지만 재배포는 금지됩니다. <br><br>
 실행 가능 운영체제 : Ubuntu 16.04 LTS 이상 <br><br>
 우분투 운영체제 버전이 Ubuntu 16.04 LTS 이하인 경우 업그레이드를 진행 하신후 마냐디완s를 실행해 주시기 바랍니다.
 # 주의 사항
 마냐디완 s PRO 프로그램을 실행하면 sysctl.conf (리눅스 커널 설정 파일), rules.v4 (iptables 설정 파일)이 /etc/backup 폴더에 백업됩니다. <br><br>
-
+마냐디완 s PRO 프로그램을 실행하면 crontab 파일이 날라갑니다. 백업해주세요.<br><br>
 이프로그램(마냐디완 s PRO)을 실행하여 발생한 피해는 책입지지 않습니다. <br><br> 적용이 완료되면 시스템을 재부팅합니다. <br><br>
     오류가있을 경우 dpkg --configure -a 입력해주세요. <br><br>
 # 실행하는 법 <br>
 wget https://dp.ael.kr/up.sh --no-check-certificate <br><br>
 sudo sh up.sh <br><br> 
-sudo cd /etc/dp/ <br><br>
-sudo sh start.sh <br><br>
 # 도움을 주신분
 <a href="https://github.com/danieluhm">다날</a> [ rules.v4 (iptables 설정 파일) 백업 및 파일 수정 방식 변경(덮어씌우는 방식에서 내용을 추가하는 방법) 제안하셨습니다. ] <br><br>
 # 적용된 서버 
--- up.sh
+++ up.sh
@@ -9,22 +9,55 @@ exit
 fi
 done
 
+apt-get update;apt-get upgrade -y;apt-get install git -y;
+
+# 업데이트 목록 갱신 및 업그레이드 가능한 모든 패키지 업그레이드, git을 설치합니다.
+
+apt-get install zram-config -y;
+
+# zram 설치 
+
+apt-get install cron -y;
+
+# cron 설치 
+
 mkdir /etc/backup/;
-cp /etc/sysctl.conf /etc/backup/;cp /etc/iptables/rules.v4 /etc/backup/
+cp /etc/sysctl.conf /etc/backup/;cp /etc/iptables/rules.v4 /etc/backup/;
 
 # sysctl.conf (리눅스 커널 설정 파일), rules.v4 (iptables 설정 파일)을 /etc/backup 폴더에 백업합니다.
-# 백업 폴더 위치 : /etc/bu/
+# 백업 폴더 위치 : /etc/backup/
 
-apt-get update 
-apt-get upgrade -y 
+git clone https://github.com/allequalit/ddos.git;
+mv dp /etc/;
 
-# 업데이트 목록 갱신 및 업그레이드 가능한 모든 패키지 업그레이드등을 수행합니다,
+# 마냐디완s PRO 소스코드 다운로드
 
-apt-get install git -y & git clone https://github.com/allequalit/ddos.git
+rm /etc/sysctl.conf;cp /etc/ddos/sysctl.conf /etc/;
 
-# git 프로그램 설치 및 아트디완s 소스코드 다운로드
+# /etc/sysctl.conf/ (리눅스 커널 설정 파일)을 덮어씌웁니다. 백업파일은 /etc/backup  폴더에있습니다.
+# 네트워크 최적화, ip스푸핑 방어, tcpbbr(https://cloud.google.com/blog/products/gcp/tcp-bbr-congestion-control-comes-to-gcp-your-internet-just-got-faster), 디도스완화가 적용되어있습니다.
 
-mv dp /etc/
-chmod +x /etc/ddos/up.sh & chmod +x /etc/ddos/start.sh
+apt-get install iptables-persistent -y;
 
-# 아트디완s 소스코드를 /etc/dp/ 디렉토리로 이동, start.sh, up.sh에 실행권한 부여
+cp /usr/share/netfilter-persistent/plugins.d/15-ip4tables /etc/init.d/iptables;
+/etc/init.d/iptables start;
+/etc/init.d/iptables flush;
+update-rc.d -f iptables defaults;
+
+# 우분투 iptables 서비스 활성화
+# insserv: warning: script ‘K01iptables’ missing LSB tags and overrides
+# insserv: warning: script ‘iptables’ missing LSB tags and overrides
+# 위와 같은 애러가 표시될 경우 게시글(https://idchowto.com/?p=31482)을 참조하여 문제를 해결해주세요.
+
+cat /etc/ddos/rules.v4 >> /etc/iptables/rules.v4;
+
+# rules.v4 (iptables 설정 파일)에 내용을 추가합니다.
+# 디도스공격 완화, 일부 C&C 서버 IP 차단, 중국 IP 아피피 차단, ICMP 차단을 적용합니다.
+
+crontab /etc/ddos/crontab 
+
+# O**, 알*** 클***, re*****es*** IP 주소에 블랙홀라우팅을 적용했습니다.
+
+reboot;
+
+# 서버 재부팅
